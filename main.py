dd0617f2893ee10e97417d532465f0bd54f4c968
Alin <34008360+Alin@users.noreply.github.com>
2020-12-17 19:29:45 +0200

    bully time

--- main.py
+++ main.py
@@ -5,17 +5,32 @@ client = commands.Bot(command_prefix = '$')
 
 client = discord.Client()
 
+nr = 0
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
+
+
 async def on_message(message):
+    global nr
     if message.author == client.user:
         return
 
-    if message.author.id == 145957053825810432:
-        await message.channel.send('Taci.')
+    if message.author.id == 145957053825810432
+        if nr == 0:
+            nr+=1
+            await message.channel.send("Taci. {}".format(message.author.mention))
+        elif nr == 1:
+            nr += 1
+            await message.channel.send('De ce incerci?')
+        elif nr == 2:
+            nr += 1
+            await message.channel.send('Ba ?')
+        else:
+            nr += 1
+            await message.channel.send('eu nu mai incerc cu asta @everyone')
 
     if message.content.startswith('$'):
         await message.channel.send('Hello!')
