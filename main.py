7c8965c9606a0bf1023799054e349da41a23bad2
Boza Rares-Dorian <rares17@pop-os.localdomain>
2020-12-17 20:10:18 +0100

    fixed command bug, cleaned code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
+import random
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '$')
-
+bullyMagnet = ['De ce incerci?', 'Ba ?','Voi il vedeti pe asta ba @everyone', 'Iesi acasa', 'Iesi', 'Nu te-ai futut cu Andone nu?']
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -14,40 +15,25 @@ async def _test(ctx, args):
 
 f = open("key.config","r")
 key = f.readline()
-
+#369108820313636865
 nr = 0
 @client.event
 async def on_message(message):
     global nr
     if message.author == client.user:
         return
-
-#bully-on
     if message.author.id == 369108820313636865:
-        if nr == 0:
-            nr += 1
-            await message.channel.send("Taci. {}".format(message.author.mention))
-        elif nr == 1:
-            nr += 1
-            await message.channel.send('De ce incerci?')
-        elif nr == 2:
-            nr += 1
-            await message.channel.send('Ba ?')
-        else:
-            nr += 1
-            await message.channel.send('eu nu mai incerc cu asta @everyone')
+        print('test')
+        await message.channel.send(random.choice(bullyMagnet))
 
-    if message.channel.id == 400674062004781056 :
+    if message.channel.id == 400674062004781056:
         await message.channel.send('Pai si tu crezi ca esti amuzant ')
 
     if f'<@!{241955978466164737}>' in message.content:
         await message.channel.send('Ce vrei ma cu terminatul ala')
 
-#    if message.content.startswith('$'):
-#        await message.channel.send('Hello!')
-
-#faza
     if 'gusi' in message.content.lower():
         await message.channel.send('Mevic?')
+    await client.process_commands(message)
 
 client.run(key)
