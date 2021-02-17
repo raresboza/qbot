f28fec961ca0ebdd922f8c136076bd0f5119cef9
giobiba <g.blanar82@gmail.com>
2021-02-17 19:12:07 +0200

    add white space

--- reddit.py
+++ reddit.py
@@ -11,14 +11,15 @@ password = redditconfig.readline().rstrip('\n')
 token = redditconfig.readline().rstrip('\n')
 
 
-reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     refresh_token= token,
-                     user_agent= "Discord bot!",
-                     username= username,
-                     password= password)
+reddit = praw.Reddit(client_id=client_id
+                     , client_secret=client_secret
+                     , refresh_token=token
+                     , user_agent="Discord bot!"
+                     , username=username
+                     , password=password)
 
 sys_random = random.SystemRandom()
+
 
 def searchSub(sub: str, isNSFW: bool):
 
@@ -27,6 +28,7 @@ def searchSub(sub: str, isNSFW: bool):
     if len(subreddit_list) == 0:
         return None
     return str(subreddit_list[0])
+
 
 def getHottestPost(sub: str, isNSFW: int):
 
@@ -38,13 +40,13 @@ def getHottestPost(sub: str, isNSFW: int):
 
     submissions = subreddit.hot(limit=5)
 
-
-    top_post = list(filter((lambda sub: not sub.stickied and re.findall("(.jpg|.png)$" ,sub.url)), submissions))
+    top_post = list(filter((lambda sub: not sub.stickied and re.findall("(.jpg|.png)$", sub.url)), submissions))
 
     if len(top_post) == 0:
         raise Exception("Couldn't find any submissions")
 
     return top_post[0].url
+
 
 def getRandomPost(sub: str,  isNSFW: int):
     subreddit = reddit.subreddit(sub)
@@ -53,20 +55,20 @@ def getRandomPost(sub: str,  isNSFW: int):
         raise Exception("This subreddit is NSFW")
 
     random_cap = random.randrange(start=0, stop=500)
-    #print(random_cap)
 
     tstart = datetime.datetime.now()
 
     submissions = subreddit.hot(limit=random_cap)
 
-    random_post = list(filter((lambda sub: not sub.stickied and re.findall("(.jpg|.png)$" ,sub.url)), submissions))[-1]
+    random_post = list(filter((lambda sub: not sub.stickied and re.findall("(.jpg|.png)$", sub.url)), submissions))[-1]
 
     tend = datetime.datetime.now()
 
     duration = (tend - tstart).total_seconds()
-    print("Request took {} seconds for searching {} posts".format(duration, random_cap));
+    print("Request took {} seconds for searching {} posts".format(duration, random_cap))
 
     return random_post.url
+
 
 if __name__ == "__main__":
     getHottestPost("prequelmemes")
